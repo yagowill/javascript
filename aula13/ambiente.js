4c7467a1730f04cbffc887c868ca6390c1810940
yagowill <99210475+yagowill@users.noreply.github.com>
2022-02-18 20:58:10 -0300

    criação aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,12 @@
+var c = 1
+/* Estrutura de repetição com teste lógico no início 
+while (c <= 500) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+//Estrutura de repetição com teste lógico no final
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
