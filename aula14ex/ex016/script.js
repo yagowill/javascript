f44fd7022d39de320e4a70cfa48528069367fef1
yagowill <99210475+yagowill@users.noreply.github.com>
2022-02-21 20:55:45 -0300

    ex016 adicionado

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,29 +1,37 @@
 function contar() {
-var txt1 = document.getElementById('txt1')
-var txt2 = document.getElementById('txt2')
-var txt3 = document.getElementById('txt3')
-var inicio = Number(txt1.value)
-var fim = Number(txt2.value)
-var passo = Number(txt3.value)
-var res = document.getElementById('res')
-if (fim == 0 ){
-    res.innerText = 'Impossível contar!'
-} else if (passo == 0) {
-    alert('Passo inválido, considerando passo 1')
-    passo = 1
-    res.innerHTML = 'Contando:'
-    document.createElement('p').setAttribute('id', 'contagem')
-    var contagem = document.getElementById('contagem')
-    for (var c = inicio ; c <= fim ; c += passo) {
-        res.innerText += `👉${c} `    
-    }
-} else {
-    res.innerHTML = '<p>Contando:</p>'
-    document.createElement('p').setAttribute('id', 'contagem')
-    var contagem = document.getElementById('contagem')
-    for (var c = inicio ; c <= fim ; c += passo) {
-        res.innerText += `👉${c} `    
-    }
+    let txt1 = document.getElementById('txt1')
+    let txt2 = document.getElementById('txt2')
+    let txt3 = document.getElementById('txt3')
+    let res = document.getElementById('res')
+    let ini = Number(txt1.value)
+    let fim = Number(txt2.value)
+    let passo = Number(txt3.value)
+    if (txt1.value.length == 0 || txt2.value.length == 0 || txt3.value.length == 0) {
+        res.innerText = 'Impossível contar, verifique os dados...'
 
-}
+    } else if (passo == 0 ) {
+        alert('considerando passo = 1')
+        passo++        
+        res.innerHTML = "Contando...<br>"
+        if (ini < fim) {
+            for (var c = ini ; c < fim ; c += passo ) {
+            res.innerText += `${c} \u{1F449}`
+            } 
+        } else {
+            for (var c = ini ; c > fim ; c -= passo ) {
+                res.innerText += `${c} \u{1F449}`
+            } 
+        } res.innerText += `\u{1F3C1} `
+    } else {
+        res.innerHTML = "Contando...<br>"
+        if (ini < fim) {
+            for (var c = ini ; c < fim ; c += passo ) {
+            res.innerText += `${c} \u{1F449}`
+            } 
+        } else {
+            for (var c = ini ; c > fim ; c -= passo ) {
+                res.innerText += `${c} \u{1F449}`
+            } 
+        } res.innerText += `\u{1F3C1} `
+    }
 }
