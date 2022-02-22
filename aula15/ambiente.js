722944943f1da42136b33dc7a2b2e9e72ba8b274
yagowill <99210475+yagowill@users.noreply.github.com>
2022-02-21 22:06:37 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,3 +1,7 @@
 let num = [5, 9, 8, 2, 4]
-
-console.log(`Nosso vetor é ${num}`)
+num.sort()
+num.push(1)
+console.log(`O vetor num possui ${num.length} posições`)
+console.log(`O primerio valor do vetor é ${num[0]}`)
+let pos = num.indexOf(3)
+console.log(`O valor 3 está na posição ${pos}`)
